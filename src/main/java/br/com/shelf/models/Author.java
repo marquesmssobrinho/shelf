0e1efbb3bd66bc208d8158a0ff42ee9a2df25bbe
Mario Alvial <mse.alvial@gmail.com>
2019-01-08 19:27:35 -0200

    Create Author entity

---
+++ src/main/java/br/com/shelf/models/Author.java
@@ -0,0 +1,42 @@
+package br.com.shelf.models;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public Author(String name) {
+        this.name = name;
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    @PreUpdate
+    private void onUpdate(){
+        this.updatedAt = LocalDateTime.now();
+    }
+}
